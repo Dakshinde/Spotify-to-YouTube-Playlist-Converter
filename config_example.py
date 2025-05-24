a81e10be6a6ba322d2d1db24c2a6b5304c62e278
Dakshinde <daksh.shinde.dhs@gmail.com>
2025-05-24 12:13:40 +0530

    Create config_example.py

---
+++ config_example.py
@@ -0,0 +1,7 @@
+# Rename this file to config.py and fill in your API credentials
+
+SPOTIFY_CLIENT_ID = "your_spotify_client_id_here"
+SPOTIFY_CLIENT_SECRET = "your_spotify_client_secret_here"
+SPOTIFY_REDIRECT_URI = "your_spotify_redirect_uri_here"
+
+GOOGLE_CLIENT_SECRET_FILE = "path_to_your_google_client_secret.json"
